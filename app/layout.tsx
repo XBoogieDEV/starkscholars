3ac8eea45e3be6209f57d9418eb1bf0e5bf4989e
Aaron <aaron@iamtechco.com>
2026-02-01 13:38:41 -0500

    Simplify layout, remove middleware temporarily for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
-import { ConvexClientProvider } from "@/components/convex-provider";
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <ConvexClientProvider>
-      {children}
-    </ConvexClientProvider>
-  );
+  return children;
 }
